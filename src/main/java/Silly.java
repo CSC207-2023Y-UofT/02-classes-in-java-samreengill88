25a2c537d2698656c0019408e637767664aec84f
samreengill <samreengill.kaur@mailutoronto.ca>
2023-06-05 15:41:20 -0400

    classes in java done I

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -11,16 +11,16 @@ import java.util.Arrays;
  * relevant readings for each task.
  */
 
-public class Silly implements Comparable<Silly>{
+public class Silly implements Comparable<Silly> {
     /**
      * 1. Java has variables known as "static" variables.
      * These are variables that exist in every instance of a class,
      * and which have the same value across all instances.
-     *
+     * <p>
      * Below we have created a static variable (signified by the keyword
      * static) named my_static. The countStatic() method makes use of this
      * static variable.
-     *
+     * <p>
      * (Relevant reading: 2.1. Static variables)
      */
     public static int my_static = 0;
@@ -79,9 +79,16 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
-
-
-
+    /**
+     * Creates a new Silly Object
+     * The constructor takes in two Strings as arguments.
+     *
+     * @param s1 first string for Silly's instance's name
+     * @param s2 second string for Silly's instance's name
+     */
+    public Silly(String s1, String s2) {
+        this.name = s1 + s2;
+    }
 
 
     public static void main(String[] args) {
@@ -116,35 +123,36 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {1, 1, 1, 1};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
 
     /**
      * 3. Whenever we try to build a string with an Object, the toString()
-     *    method is called. This is equivalent to the __str__ method in Python.
-     *
-     *    (Relevant reading: 2.3. toString)
-     *
+     * method is called. This is equivalent to the __str__ method in Python.
+     * <p>
+     * (Relevant reading: 2.3. toString)
+     * <p>
      * Return the String representation of this Silly object (i.e. its name).
      *
      * @return the name of this Silly.
      */
     @Override
-    public String toString(){
+    public String toString() {
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
      * 4. We can also override the equals() method, which is the equivalent of
-     *    Python's __eq__; however, unlike Python, this is called when we use
-     *    .equals() rather than == (which checks for ID equality in Java).
+     * Python's __eq__; however, unlike Python, this is called when we use
+     * .equals() rather than == (which checks for ID equality in Java).
+     * <p>
+     * (Relevant reading: 2.4. Equality)
      *
-     *    (Relevant reading: 2.4. Equality)
-     *
-     * @param o   the object to compare to
-     * @return    whether o is a Silly object with the same name as this.
+     * @param o the object to compare to
+     * @return whether o is a Silly object with the same name as this.
      */
     @Override
     public boolean equals(Object o) {
@@ -153,7 +161,7 @@ public class Silly implements Comparable<Silly>{
          *                We've started it by checking the type of o for you.
          *                You just need to return true if the names are equal.
          */
-        if (!(o instanceof Silly)){
+        if (!(o instanceof Silly)) {
             return false;
         }
 
@@ -161,7 +169,11 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+
+        return this.name.equals(other.name);
+
     }
+
 
     /**
      * 5. If we want to compare two objects, we have to do two things:
@@ -194,6 +206,15 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        if(this.name.length() > other.name.length()){
+            return 1;
+        } else if (this.name.length() < other.name.length()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+
     }
 
     /*
